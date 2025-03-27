82a30586ea06361aa9271979ba09f6c208678cdd
“Sombrerito” <furrfran@gmail.com>
2025-03-27 20:26:29 -0300

    Ejercicio terminado gracias a Lab de hoy

--- Programas AyEDII/lab01-kickstart/lab01/ej0/fixstring.c
+++ Programas AyEDII/lab01-kickstart/lab01/ej0/fixstring.c
@@ -2,43 +2,43 @@
 #include <assert.h>
 
 #include "fixstring.h"
-#define WORD_MAX_LENGHT 30
+#define WORD_MAX_LENGTH 30
 
-unsigned int fstring_length(fixstring s) {
+unsigned int fstring_length(fixstring s){
     int char_count = 0;
-    for(int i=0;i<WORD_MAX_LENGHT;i++){
-        if (s[i]=='\0'){
-            break;
-        }
-        else{
-            char_count++;
-        }
+    int i = 0;
+    while(s[i] != '\0'){
+        char_count++;
+        i++;
     }
     return char_count;
 }
 
 bool fstring_eq(fixstring s1, fixstring s2) {
-    int eq_char = 0;
-    for(int i=0;i<WORD_MAX_LENGHT;i++){
-        if (s1[i]==s2[i]){
-            eq_char++;
-        }
-        else{
-            return false;
-            break;
-        }
+    bool res = true;
+    int i = 0;
+    int l_s1 = fstring_length(s1);
+    int l_s2 = fstring_length(s2);
+    while((i<WORD_MAX_LENGTH)&&((l_s1!=l_s2)||(s1[i]!=s2[i]))){
+        res = false;
+        i++;
     }
-    return true;
+    return res;
 }
 
 bool fstring_less_eq(fixstring s1, fixstring s2) {
     bool res = true;
-    for (int i=0;i<WORD_MAX_LENGHT;i++){
-        else if(s1[i]>s2[i]){
+    int i = 0; 
+    while((s1[i]!='\0')&&(s2[i]!='\0') && res){
+        if (s1[i]!=s2[i]){
             res = false;
         }
+        i++;
+    }
+    if(s1[i]>s2[i]||(s2[i]=='\0'&&s1[i]!='\0')){
+        res = true;
     }
     return res;
-}
+    }
 
 // Se puede corregir que aparezca lo de va antes o después en los iguales?
